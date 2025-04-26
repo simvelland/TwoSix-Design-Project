98ff649514bd5e547d8c30b3d26e628e508cd87a
simvelland <125514072+simvelland@users.noreply.github.com>
2025-04-26 02:03:23 -0400

    file transfer

---
+++ Prototype 1/PHASE 1/app.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+import pandas as pd
+
+model = joblib.load('trained_gbr_model.pkl')
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/predict/")
+def predict(data: dict):
+    try:
+        features = np.array([[
+            data["Solar_Irradiance"], 
+            data["Temperature_2m"],
+            data["Relative_Humidity_2m"],
+            data["Wind_Speed_2M"],
+            data["Surface_Pressure"],
+            data["Cloud_Amount"]
+        ]])
+
+        prediction = model.predict(features)[0]
+
+        return {"predicted_daily_solar_power": prediction}
+
+    except Exception as e:
+        return {"error": str(e)}
